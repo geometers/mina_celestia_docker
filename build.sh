1e46b28bec4b633b8ffb6e4d80ff1aadeb5a9f56
Koh Wei Jie <contact@kohweijie.com>
2024-07-31 16:20:46 -0700

    docker build up to blob-stream-inclusion/blobstream/script cargo run --release

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Change to the directory that this script lives in
+cd $(dirname "$0")
+
+# Clone private repos
+
+mkdir -p private_repos
+
+cd private_repos
+rm -rf blob-stream-inclusion
+git clone git@github.com:geometers/blob-stream-inclusion.git
+cd blob-stream-inclusion
+rm -rf .git
+cd ..
+tar -czvf blob-stream-inclusion.tar.gz blob-stream-inclusion
+cd ..
+
+# Build the docker image
+echo "Building docker image..."
+docker build -t mina_celestia .
---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Change to the directory that this script lives in
+cd $(dirname "$0")
+
+# Run the docker image
+docker run mina_celestia
